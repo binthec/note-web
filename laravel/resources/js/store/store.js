6eb7baab3a1489b56288e534e38d226e38e22957
binthec <binthec@gmail.com>
2022-11-12 22:06:32 +0900

    軽微な修正

--- laravel/resources/js/store/store.js
+++ laravel/resources/js/store/store.js
@@ -3,13 +3,10 @@ import Vuex from 'vuex';
 
 import engi_list from './modules/edit_list';
 
-
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
+export const store = new Vuex.Store({
    modules: {
        engi_list,
    }
 });
-
-export default store;
