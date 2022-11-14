c192f763c0eeab5e735287338f328db8c679e9ee
ccIshibashi <mn.ishibashi@cloudcircus.co.jp>
2022-11-14 12:47:32 +0900

    環境を複数作るときのことを考えて、ポート番号をちょっと変更

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            'database' => env('DB_DATABASE', 'sanjo'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'secret'),
             'charset' => 'utf8',
