8dae78de897f9f3287c4a23cb29958bb53d5cf0c
binthec <binthec@gmail.com>
2023-02-03 20:33:56 +0900

    アイテムのダウンロード機能追加

--- laravel/resources/js/components/item/ItemDownload.vue
+++ laravel/resources/js/components/item/ItemDownload.vue
@@ -1,23 +1,11 @@
 <template>
     <default-layout>
-
         <list-header slot="page_header"
                      :header-icon="'nav-icon fas fa-file-download'"
                      :page-title="'アイテムダウンロード'"
         ></list-header>
 
-        <template v-slot:card_body>
-            <div class="card-body row">
-                <div class="col-4" v-for="(item, index) in list.data">
-                    <div class="piece">
-                        <span class="item-title">{{ item.title }}</span>
-                        <div class="item-img">
-                            <img :src="getFilePath(item.first_cate, item.uuid)">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </template>
+        <download-body slot="card_body"></download-body>
 
         <Paginator slot="card_footer"
                    :list="list"
@@ -31,17 +19,16 @@
 import DefaultLayout from "../common/layout/DefaultLayout";
 import ListHeader from "../common/parts/ListHeader";
 import Paginator from "../common/parts/Paginator";
+import DownloadBody from "./download/DownloadBody";
 
 // store
 import {mapState, mapActions, mapMutations} from "vuex";
-
-// script
-import {getImageSvgPath} from "../../script/item"
 
 export default {
     name: "ItemDownload",
 
     components: {
+        DownloadBody,
         DefaultLayout,
         ListHeader,
         Paginator
@@ -73,47 +60,10 @@ export default {
         ...mapActions('item/download', [
             'getList',
         ]),
-
-        getFilePath(first_cate, uuid) {
-            return getImageSvgPath(first_cate, uuid);
-        },
     }
 }
 </script>
 
-<style lang="scss" scoped>
-@import "../../../sass/variables";
+<style scoped>
 
-.piece {
-    border: solid 2px $gray-border;
-    border-radius: 15px;
-    margin-bottom: 10px;
-    min-height: 180px;
-    background-color: #ffffff;
-    position: relative;
-
-    .item-title {
-        display: inline-block;
-        margin: 5px 35px 0 8px;
-        padding-right: 5px;
-        position: absolute;
-        z-index: 10;
-        background: rgba(255, 255, 255, 0.8);
-        font-weight: 700;
-    }
-
-    .item-img {
-        position: absolute;
-        display: flex;
-        width: 100%;
-        height: 100%;
-        padding: 40px 5px 5px 5px;
-
-        img {
-            margin: auto;
-            width: 100%;
-            height: 100%;
-        }
-    }
-}
 </style>
---
+++ laravel/resources/js/components/item/download/DownloadModal.vue
@@ -0,0 +1,96 @@
+<template>
+    <modal name="download-item-modal"
+           :width="'70%'"
+           :height="'auto'"
+    >
+        <div class="modal-header bg-info">
+            <h4 class="item-title">{{ item.title }}</h4>
+        </div>
+        <div class="modal-body">
+            <div class="wrapper">
+                <div class="item-img text-center" id="download-img">
+                    <img :src="getFilePath(item.first_cate, item.uuid)">
+                </div>
+            </div>
+            <span>ダウンロード：</span>
+            <button type="button" class="btn btn-info" @click="downloadPng(item.uuid)">PNG</button>
+            <button type="button" class="btn btn-info" @click="downloadSvg">SVG</button>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-dark" @click="hideModal">閉じる</button>
+        </div>
+    </modal>
+</template>
+
+<script>
+import {getImageSvgPath} from "../../../script/item";
+import * as htmlToImage from 'html-to-image';
+import {toPng, toJpeg, toBlob, toPixelData, toSvg} from 'html-to-image';
+import * as download from "downloadjs";
+
+export default {
+    name: "DownloadModal",
+
+    props: {
+        item: {
+            default: {}
+        }
+    },
+
+    methods: {
+        hideModal() {
+            this.$modal.hide('download-item-modal');
+        },
+
+        getFilePath(first_cate, uuid) {
+            return getImageSvgPath(first_cate, uuid);
+        },
+
+        downloadPng(uuid) {
+            let node = document.getElementById('download-img');
+            htmlToImage
+                .toPng(node)
+                .then(function (dataUrl) {
+                    download(dataUrl, uuid);
+                })
+                .catch(function (error) {
+                    console.error('oops, something went wrong!', error);
+                });
+        },
+
+        downloadSvg() {
+            download(this.getFilePath(this.item.first_cate, this.item.uuid));
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.item-title {
+    font-weight: 700;
+}
+
+.modal-body {
+    background-color: #eee;
+
+    .wrapper {
+        padding: 20px 20px 60px;
+        margin: auto;
+
+        .item-img {
+            background-color: #fff;
+            text-align: center;
+            padding: 15px;
+        }
+    }
+
+    span {
+        font-weight: 500;
+    }
+
+    button {
+        margin-right: 10px;
+    }
+
+}
+</style>
