2b08ba3faab2dc1af81ef9a3ea8c5ef5a5781bf6
binthec <binthec@gmail.com>
2024-03-11 18:07:03 +0900

    デザインの微調整

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -48,13 +48,16 @@
                 font-size: 84px;
             }
 
-            .links > a {
+            .notice, .links > a {
                 color: #636b6f;
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
+            }
+
+            .links {
                 text-transform: uppercase;
             }
 
@@ -81,19 +84,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Friends Note Web
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <p class="notice">Please Login.</p>
             </div>
         </div>
     </body>
